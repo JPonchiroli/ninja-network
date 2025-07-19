049697111bba36e29c001b477ec5a9be81d9314f
JPonchiroli <joaopedroponqueroli@gmail.com>
2025-07-19 17:03:57 -0300

    feat: lesson #5 create layout and slots

--- resources/views/ninjas/index.blade.php
+++ resources/views/ninjas/index.blade.php
@@ -1,23 +1,12 @@
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>Ninja Network | Home</title>
-    </head>
-    <body>
-        <h2>Currently Available Ninjas</h2>
+ <x-layout>
+    <h2>Currently Available Ninjas</h2>
 
-        @if ($greeting == "Hello")
-            <p>Hi from inside the if statement</p>
-        @endif
-
-        <ul>
-            @foreach($ninjas as $ninja)
-                <li>
-                    <p>{{ $ninja["name"] }}</p>
-                    <a href="/ninjas/{{  $ninja['id'] }}">View Details</a>
-                </li>
-            @endforeach
-        </ul>
-    </body>
-</html>
+    <ul>
+        @foreach($ninjas as $ninja)
+        <li>
+            <p>{{ $ninja["name"] }}</p>
+            <a href="/ninjas/{{  $ninja['id'] }}">View Details</a>
+        </li>
+        @endforeach
+    </ul>
+</x-layout>
--- resources/views/ninjas/show.blade.php
+++ resources/views/ninjas/show.blade.php
@@ -1,10 +1,3 @@
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>Ninja Network</title>
-    </head>
-    <body>
+<x-layout>
         <h2>Ninja Id - {{ $id }}</h2>
-    </body>
-</html>
+</x-layout>
