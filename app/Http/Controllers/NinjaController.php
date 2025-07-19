7ea6304095cb55e3ef5ce4e4347c81235636b46a
JPonchiroli <joaopedroponqueroli@gmail.com>
2025-07-19 20:53:02 -0300

    feat: lesson #22 adding a flash card when create or delete a ninja

--- app/Http/Controllers/NinjaController.php
+++ app/Http/Controllers/NinjaController.php
@@ -36,13 +36,13 @@ class NinjaController extends Controller
 
         Ninja::create($validated);
 
-        return redirect()->route('ninjas.index');
+        return redirect()->route('ninjas.index')->with('success', 'Ninja Created!');
     }
 
     public function destroy($id) {
         $ninja = Ninja::findOrFail($id);
         $ninja->delete();
 
-        return redirect()->route('ninjas.index');
+        return redirect()->route('ninjas.index')->with('success', 'Ninja Deleted!');
     }
 }
